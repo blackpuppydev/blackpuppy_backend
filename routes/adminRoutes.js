00243e76ca0dfc86504a98d4ebe6517d601b3421
Nattawut.c <blackpuppy.dev@gmail.com>
2025-06-20 12:35:16 +0700

    commit 20/06/2025 - add add customer and delete customer

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,5 +5,17 @@ const { verifySupabaseToken } = require("../middlewares/authMiddlewares");
 
 router.get("/dashboard", verifySupabaseToken, adminControllers.getDashboard);
 router.get("/display", verifySupabaseToken, adminControllers.getDisplay);
+router.post("/add-customer", verifySupabaseToken, adminControllers.addCustomer);
+router.put(
+  "/edit-customer/:id",
+  verifySupabaseToken,
+  adminControllers.updateCustomer
+);
+
+router.delete(
+  "/delete-customer/:id",
+  verifySupabaseToken,
+  adminControllers.deleteCustomer
+);
 
 module.exports = router;
